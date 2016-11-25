6e64835f0236bb182bb97c6e533a775953853249
zyh <zyh@dhcp-8-235.nay.redhat.com>
2016-11-25 11:36:48 +0800

    modify the deadlock_server.py

--- deadlock_server.py
+++ deadlock_server.py
@@ -1,6 +1,8 @@
 # !/usr/bin/env python
 # Server about deadlock
 
+# Just a instance
+"""
 import socket, traceback
 host = ''
 port = 51423
@@ -40,3 +42,51 @@ while 1:
 		raise
 	except:
 		traceback.print_exc()
+
+"""
+
+
+
+
+
+import socket, traceback
+
+host = ''
+port = 51423
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((host, port))
+s.listen(1)
+
+while 1:
+	try:
+		clientsock, clientaddr = s.accept()
+	except KeyboardInterrupt:
+		raise
+	except:
+		trace.print_exc()
+		continue
+
+	try:
+		print "The client: ", clientaddr
+		while 1:
+			
+			data = clientsock.recv(4096)
+			if not len(data):
+				break
+			clientsock.sendall(data)
+	except (KeyboardInterrupt, SystemExit):
+		raise
+	except:
+		traceback.print_exc()
+
+	try:
+		clientsock.close()
+	except KeyboardInterrupt:
+		raise
+	except:
+		traceback.print_exc()
+
+
+
